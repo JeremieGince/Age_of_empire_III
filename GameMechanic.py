59bf9fb85660ce165030f4f18ea1c4f2f8cb03bd
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-02-04 12:02:53 -0500

    Update GameMechanic.py

--- GameMechanic.py
+++ GameMechanic.py
@@ -1,19 +1,37 @@
 import os, time
-import pyautogui
-import pytesseract
-import cv2
-import numpy as np
-from PIL import Image
-from GUIParser import IGameAoE3GUIParser
+from GUIParser import InGameAoE3GUIParser
 
 
 class GameEnvironment:
     def __init__(self, botInstance):
         self._iteration: int = 0
-        self.InGameGui = IGameAoE3GUIParser()
 
         self._bot = botInstance
         self._bot.setGameEnv(self)
+
+        self.InGameGui = None
+
+    @property
+    def Iteration(self) -> int:
+        return self._iteration
+
+    def run(self):
+        self._bot.onStart()
+        time.sleep(5)
+        while self.Iteration <= 1_000:
+            print(f"iteration: {self.Iteration}")
+            self._bot.onStep()
+
+            self._iteration += 1
+        self._bot.onEnd()
+
+
+class AoE3Environment(GameEnvironment):
+    def __init__(self, botInstance):
+        super(AoE3Environment, self).__init__(botInstance)
+        self.InGameGui = InGameAoE3GUIParser()
+
+        self.GuiItems_updates: dict = {name: 0 for name, _ in self.InGameGui.items()}
 
         self._food: int = 0
         self._wood: int = 0
@@ -22,128 +40,58 @@ class GameEnvironment:
 
     @property
     def Food(self) -> int:
+        if self.GuiItems_updates["FoodSlot"] < self.Iteration:
+            self.updateFoodSlot()
         return self._food
 
     @property
     def Wood(self) -> int:
+        if self.GuiItems_updates["WoodSlot"] < self.Iteration:
+            self.updateWoodSlot()
         return self._wood
 
     @property
     def Gold(self) -> int:
+        if self.GuiItems_updates["GoldSlot"] < self.Iteration:
+            self.updateGoldSlot()
         return self._gold
 
     @property
     def HmVillager(self) -> int:
+        if self.GuiItems_updates["HmIdleVillagerSlot"] < self.Iteration:
+            self.updateFoodSlot()
         return self._hmVillager
 
-    @property
-    def Iteration(self) -> int:
-        return self._iteration
+    def updateFoodSlot(self):
+        foodStr = repr(self.InGameGui["FoodSlot"])
+        # self.InGameGui["FoodSlot"].show()
+        if foodStr:
+            self._food = int(foodStr)
+            self.GuiItems_updates["FoodSlot"] = self.Iteration
 
-    def updateFood(self):
-        # foodPos = pyautogui.locateOnScreen("Icons/Food.PNG", confidence=0.9, grayscale=True)
-        # foodText = pyautogui.screenshot(region=(foodPos[0] + foodPos[2], foodPos[1], foodPos[2] * 1.5, foodPos[3]))
-        # foodText = -cv2.cvtColor(np.array(foodText), cv2.COLOR_RGB2GRAY)
-        # text = pytesseract.image_to_string(Image.fromarray(foodText), config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-        # for digit in text:
-        #     if digit not in GameEnvironment.digitsNumber:
-        #         text.replace(digit, '')
-        # if text:
-        #     try:
-        #         self._food = int(float(text))
-        #     except Exception:
-        #         pass
-        self._food = int(str(self.InGameGui["FoodSlot"]))
+    def updateWoodSlot(self):
+        woodStr = repr(self.InGameGui["WoodSlot"])
+        # self.InGameGui["WoodSlot"].show()
+        if woodStr:
+            self._food = int(woodStr)
+            self.GuiItems_updates["WoodSlot"] = self.Iteration
 
-    def updateWood(self):
-        woodPos = pyautogui.locateOnScreen("Icons/Wood.PNG", confidence=0.9, grayscale=True)
-        woodText = pyautogui.screenshot(region=(woodPos[0] + woodPos[2], woodPos[1], woodPos[2] * 1.5, woodPos[3]))
-        woodText = -cv2.cvtColor(np.array(woodText), cv2.COLOR_RGB2GRAY)
-        text = pytesseract.image_to_string(Image.fromarray(woodText), config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-        # for digit in text:
-        #     if digit not in GameEnvironment.digitsNumber:
-        #         text.replace(digit, '')
-        if text:
-            try:
-                self._wood = int(float(text))
-            except Exception:
-                pass
+    def updateGoldSlot(self):
+        hmVillagerStr = repr(self.InGameGui["GoldSlot"])
+        # self.InGameGui["GoldSlot"].show()
+        if hmVillagerStr:
+            self._food = int(hmVillagerStr)
+            self.GuiItems_updates["GoldSlot"] = self.Iteration
 
-    def updateGold(self):
-        goldPos = pyautogui.locateOnScreen("Icons/Gold.PNG", confidence=0.9, grayscale=True)
-        goldText = pyautogui.screenshot(region=(goldPos[0] + goldPos[2], goldPos[1], goldPos[2] * 1.5, goldPos[3]))
-        goldText = -cv2.cvtColor(np.array(goldText), cv2.COLOR_RGB2GRAY)
-        text = pytesseract.image_to_string(Image.fromarray(goldText), config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-        # for digit in text:
-        #     if digit not in GameEnvironment.digitsNumber:
-        #         text.replace(digit, '')
-        if text:
-            try:
-                self._gold = int(float(text))
-            except Exception:
-                pass
-        cv2.imshow("dodu", np.array(goldText))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-
-    def updateHmVillager(self):
-        resourcePos = pyautogui.locateOnScreen(f"Icons/Villagers.PNG", confidence=0.9, grayscale=True)
-        resourceText = pyautogui.screenshot(region=(resourcePos[0] + resourcePos[2], resourcePos[1], resourcePos[2] * 1.5, resourcePos[3]))
-        resourceText = -cv2.cvtColor(np.array(resourceText), cv2.COLOR_RGB2GRAY)
-        text = pytesseract.image_to_string(Image.fromarray(resourceText), config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-        for digit in text:
-            if digit not in GameEnvironment.digitsNumber:
-                text.replace(digit, '')
-        if text:
-            try:
-                self._hmVillager = int(float(text))
-            except Exception:
-                pass
-        # cv2.imshow("dodu", np.array(resourceText))
-        # if cv2.waitKey(25) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
-
-    def updateResource(self, resourceName: str):
-        resourcePos = pyautogui.locateOnScreen(f"Icons/{resourceName}.PNG", confidence=0.9, grayscale=True)
-        resourceText = pyautogui.screenshot(region=(resourcePos[0] + resourcePos[2], resourcePos[1], resourcePos[2] * 1.5, resourcePos[3]))
-        resourceText = -cv2.cvtColor(np.array(resourceText), cv2.COLOR_RGB2GRAY)
-        text = pytesseract.image_to_string(Image.fromarray(resourceText))
-        for digit in text:
-            if digit not in GameEnvironment.digitsNumber:
-                text.replace(digit, '')
-        if text:
-            try:
-                self._food = int(float(text))
-            except Exception:
-                pass
-        cv2.imshow("dodu", np.array(resourceText))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-
-    def updateResources(self):
-        self.updateFood()
-        # self.updateWood()
-        # self.updateGold()
-        # self.updateHmVillager()
-        print(f"Food: {self.Food}, Wood: {self.Wood}, Gold: {self.Gold}, HmVillager: {self.HmVillager}")
-
-    def run(self):
-        self._bot.onStart()
-        time.sleep(5)
-        while self.Iteration <= 1_000:
-            print(f"iteration: {self.Iteration}")
-            try:
-                self.updateResources()
-            except TypeError as err:
-                print(err)
-
-            self._bot.onStep()
-
-            self._iteration += 1
-        self._bot.onEnd()
+    def updateHmVillagerSlot(self):
+        hmVillagerStr = repr(self.InGameGui["HmIdleVillagerSlot"])
+        # self.InGameGui["HmIdleVillagerSlot"].show()
+        if hmVillagerStr:
+            self._food = int(hmVillagerStr)
+            self.GuiItems_updates["HmIdleVillagerSlot"] = self.Iteration
 
 
-GameEnv = GameEnvironment
+AoE3Env = AoE3Environment
 
 
 class BotAi:

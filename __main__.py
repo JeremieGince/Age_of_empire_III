60188ab2af5e377e8e4a2cc49a332eb53540a3a5
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-02-04 16:17:19 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -24,26 +24,6 @@ if __name__ == '__main__':
     # time.sleep(60)
     time.sleep(1)
 
-    # icon_pos = pyautogui.locateOnScreen("Buttons/Play.PNG")
-    # print("icon pos: ", (icon_pos[0], icon_pos[1]))
-    # pyautogui.moveTo(icon_pos[0]+10, icon_pos[1]+10)
-    # time.sleep(1)
-    # pyautogui.leftClick(icon_pos[0]+10, icon_pos[1]+10)
-
-    # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-    # while True:
-    #     foodPos = pyautogui.locateOnScreen("Icons/Food.PNG")
-    #     print(f"FoodPos: {foodPos}")
-    #     foodText = pyautogui.screenshot(region=(foodPos[0]+foodPos[2], foodPos[1], foodPos[2]*1.5, foodPos[3]))
-    #     foodText = -cv2.cvtColor(np.array(foodText), cv2.COLOR_RGB2GRAY)
-    #     text = pytesseract.image_to_string(Image.fromarray(foodText))
-    #     print(f"text: {text}")
-    #     cv2.imshow("dodu", np.array(foodText))
-    #     if cv2.waitKey(25) & 0xFF == ord('q'):
-    #         cv2.destroyAllWindows()
-    #         break
-
     Jarex = Bot("Jarex")
     gameEnv = AoE3Env(Jarex)
     gameEnv.run()

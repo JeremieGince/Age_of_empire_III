eee2dfb18207c8eba15d1f4c7c1b5f3a4c48d700
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-02-08 23:32:46 -0500

    Update Hotkeys.py

--- Hotkeys.py
+++ Hotkeys.py
@@ -1,8 +1,51 @@
 import pyautogui
-from Units import Unit, Units
+import time
+
+tempGroup = 9
+
+HandleToHotKeys: dict = {
+    # Find Unit Hotkeys
+    "Find idle Villager": ["."],
+    "Find Explorer": ["é"],
+    "Find idle Military": [","],
+    "Find Home City": ["h"],
+    "Find Town Center": ["t"],
+    "Find Wagon": [";"],
+    "Find Mill": ["CTRL", "i"],
+    "Find Market": ["CTRL", "m"],
+
+    # Building Construction Hotkeys
+    "Build Town Center": ["g"],
+    "Build Market": ["m"],
+    "Build Mill": ["i"],
+
+    # Selection Group Hotkeys
+    "Select temp Group": [str(tempGroup)],
+    "Create temp Group": ["CTRL", str(tempGroup)],
+    "Create Group 9": ["CTRL", "9"],
+
+     # Town Center Hotkeys
+    "Train Villager": ["v"],
+}
 
 
+def sleepKeys(func):
+    time.sleep(0.1)
+    re = func
+    time.sleep(0.7)
+    return re
+
+
+@sleepKeys
+def SendHotKeys(handle: str):
+    assert handle in HandleToHotKeys
+    pyautogui.hotkey(*HandleToHotKeys[handle])
+
+
+@sleepKeys
 def Find_idle_Villager():
-    pyautogui.hotkey(".")
-    return Unit("Villager", "")
+    pyautogui.hotkey(*HandleToHotKeys["Find idle Villager"])
+
+
+
 
